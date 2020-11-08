1328b0b83ea235641849dee2d8f0ff1d81f585ba
ymd-stella <world.applepie@gmail.com>
2020-11-08 20:32:58 +0900

    feed IMU data

--- src/openvslam/system.cc
+++ src/openvslam/system.cc
@@ -277,7 +277,7 @@ Mat44_t system::feed_RGBD_frame(const cv::Mat& rgb_img, const cv::Mat& depthmap,
     return cam_pose_cw;
 }
 
-void system::feed_IMU_data(const imu::data& imu_data) {
+void system::feed_IMU_data(const std::shared_ptr<imu::data>& imu_data) {
     tracker_->queue_IMU_data(imu_data);
 }
 
